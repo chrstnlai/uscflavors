52f55ab96bf44125c143422c7351cfd5e8b52b98
jhene <ekuwem@usc.edu>
2026-04-13 14:01:02 -0700

    Design credits.

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,7 +3,7 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer className="site-footer">
-      <span className="site-footer-center">Website designed and built by [Jhene Ekuwem, Cohort S'25], [Olivia Nazari, Cohort F'25], [Christine Lai, Cohort F'25]</span>
+      <span className="site-footer-center">Website designed and built by [Jhene Ekuwem, Cohort S'25], [Olivia Nazari, Cohort F'25], [Christine Lai, Cohort S'24]</span>
     </footer>
   );
 }
